ce5c7d5a3b8b5c055ef6d14265d10bf819b1f1b8
Ashik Varma <varmaashik@gmail.com>
2020-02-17 15:43:20 +0530

    Added music

--- addon.js
+++ addon.js
@@ -1,15 +1,19 @@
 
 
-// var sound = new Howl({
-//   src: ['lovespell.mp3']
-// });
+var sound = new Howl({
+  src: ['lovespell.mp3'],
+  autoplay:true,
+  loop:true
+});
 
-// // Clear listener after first call.
-// sound.once('load', function(){
-//   sound.play();
-// });
+// Clear listener after first call.
+sound.once('load', function(){
+  sound.play();
+});
 
-// // Fires when the sound finishes playing.
-// sound.on('end', function(){
-//   console.log('Finished!');
-// });
+// Fires when the sound finishes playing.
+sound.on('end', function(){
+  console.log('Finished!');
+});
+
+
