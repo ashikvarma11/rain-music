e88211164c3a33fbd161db4097a568ae1fa0a2f0
ashikvarma11 <varmaashik@gmail.com>
2020-07-20 17:21:48 +0530

    Added heavy rain music and seekbar

--- webjs.js
+++ webjs.js
@@ -1,8 +1,32 @@
-let scene,camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount = 15000,dropImage;
+let scene,camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount = 500,dropImage;
+count=0;
+let range = document.getElementById('rainrange');
+var mouse = new THREE.Vector2();
+var mousePressed = false;
+document.addEventListener('change',(event)=>{
+    rainCount = event.target.value;
+    heavyRainSound.stop();
+    rainSound.play();
+    if (rainCount> 4000) {
+      console.log(rainCount)
+      rainSound.stop();
+      heavyRainSound.play();
+    }
+    init();
+});
 
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+function onDocumentMouseMove( event ) {
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  // rain.rotation.x = mouse.x;
+  rain.rotation.y = mouse.y;
+}  
 
     function init() {
       scene = new THREE.Scene();
+      
       camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
       camera.position.z = 104.5;
       camera.rotation.x = 1.16;
@@ -15,7 +39,6 @@ let scene,camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount
       scene.add(directionalLight);
       flash = new THREE.PointLight(0x062d89, 30, 300 ,1.7);
       flash.position.set(200,300,100);
-      console.log(flash)
       scene.add(flash);
       renderer = new THREE.WebGLRenderer();
       scene.fog = new THREE.FogExp2(0x1b1c29, 0.002);
@@ -24,6 +47,8 @@ let scene,camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount
       document.body.appendChild(renderer.domElement);
       rainGeo = new THREE.Geometry();
 
+
+     
       for(let i=0;i<rainCount;i++) {
         rainDrop = new THREE.Vector3(
           Math.random() * 400 -200,
@@ -34,15 +59,14 @@ let scene,camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount
         rainDrop.velocity = 0;
         rainGeo.vertices.push(rainDrop);
       }
-        var dropletTexture = new THREE.TextureLoader().load( './bubble.png' );
+      
          rainMaterial = new THREE.PointsMaterial({
-        size:0.2,
-        transparent: true,
-        map:dropletTexture
+          color: 0xaaaaaa,
+          size:0.2,
+          transparent: true
       });
       
       rain = new THREE.Points(rainGeo,rainMaterial);
-      console.log(rain)
       scene.add(rain);
    
       let loader = new THREE.TextureLoader();
@@ -122,12 +146,16 @@ song.once('load', function(){
 });
 
 var rainSound = new Howl({
-  src: ['assets/music/light-rain-thunder.mp3',
-  'assets/music/more-thunder.mp3'],
+  src: ['assets/music/light-rain-thunder.mp3'],
   autoplay:true,
-
   loop:true
 });
+var heavyRainSound = new Howl({
+  src: ['assets/music/heavy-rain-daniel_simon.mp3'],
+  autoplay:false,
+  loop:true
+});
+
 
 // Clear listener after first call.
 rainSound.once('load', function(){
@@ -155,111 +183,6 @@ function changeColor(){
 flash.color.setHex(colors[idx+1]);
 ambient.color.setHex(colors[idx+1]);
 directionalLight.color.setHex(colors[idx+1]);
-idx=idx+1
+idx=idx+1;
 }
 
-
-//Slider Color Change
-
-var inputRange = document.getElementsByClassName('range')[0],
-    maxValue = 100, // the higher the smoother when dragging
-    speed = 5,
-    currValue, rafID;
-
-// set min/max value
-inputRange.min = 0;
-inputRange.max = maxValue;
-
-// listen for unlock
-function unlockStartHandler() {
-    // clear raf if trying again
-    window.cancelAnimationFrame(rafID);
-    
-    // set to desired value
-    currValue = +this.value;
-}
-
-function unlockEndHandler() {
-    
-    // store current value
-    currValue = +this.value;
-    
-    // determine if we have reached success or not
-    if(currValue >= maxValue) {
-        successHandler();
-    }
-    else {
-        rafID = window.requestAnimationFrame(animateHandler);
-    }
-}
-
-// handle range animation
-function animateHandler() {
-
-    // calculate gradient transition
-    var transX = currValue - maxValue;
-    
-    // update input range
-    inputRange.value = currValue;
-
-    //Change slide thumb color on mouse up
-    if (currValue < 20) {
-        inputRange.classList.remove('ltpurple');
-    }
-    if (currValue < 40) {
-        inputRange.classList.remove('purple');
-    }
-    if (currValue < 60) {
-        inputRange.classList.remove('pink');
-    }
-    
-    // determine if we need to continue
-    if(currValue > -1) {
-      window.requestAnimationFrame(animateHandler);   
-    }
-    
-    // decrement value
-    currValue = currValue - speed;
-}
-
-// handle successful unlock
-function successHandler() {
-  alert('Unlocked');
-};
-
-// bind events
-// inputRange.addEventListener('mousedown', unlockStartHandler, false);
-// inputRange.addEventListener('mousestart', unlockStartHandler, false);
-// inputRange.addEventListener('mouseup', unlockEndHandler, false);
-// inputRange.addEventListener('touchend', unlockEndHandler, false);
-
-// move gradient
-var defaultRainCount = 1500;
-var totalDroplets = rain.geometry.vertices;
-console.log(totalDroplets)
-inputRange.addEventListener('input', function() {
-  // console.log(defaultRainCount,this.value)
-    //Change slide thumb color on way up    
-
-    if (this.value > 20) {
-        inputRange.classList.add('ltpurple');
-    }
-    if (this.value > 40) {
-        inputRange.classList.add('purple');
-    }
-    if (this.value > 60) {
-        console.log(">60", rain.geometry.vertices)
-        inputRange.classList.add('pink');
-    }
-
-    //Change slide thumb color on way down
-    if (this.value < 20) {
-        inputRange.classList.remove('ltpurple');
-    }
-    if (this.value < 40) {
-        inputRange.classList.remove('purple');
-    }
-    if (this.value < 60) {
-        inputRange.classList.remove('pink');
-    }
-});
